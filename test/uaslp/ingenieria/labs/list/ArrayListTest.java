2692b6b2e64ae445180a224241c445bc489b20aa
alejandra sanchez barrera <A310401@alumnos.uaslp.mx>
2021-04-23 12:08:46 -0500

    Unit Test Intro

--- test/uaslp/ingenieria/labs/list/ArrayListTest.java
+++ test/uaslp/ingenieria/labs/list/ArrayListTest.java
@@ -1,2 +1,38 @@
-package uaslp.ingenieria.labs.list;public class ArrayList {
-}
+package uaslp.ingenieria.labs.list;
+
+import org.junit.jupiter.api.Test;
+
+/*public class ArrayListTest {
+
+    //biblioteca -> código ya hecho que podamos utilizar
+    //framework -> JUnit es como otra biblioteca que se añade en el proyecto, pero como hace cosas por si misma,
+    // algo corre, y así deja de ser una biblioteca
+    //jar -> empaquetado de código java
+    //Aserciones -> permiten validar que la ejecución fue correcta
+    //expresivo -> que al leer el lenguage se entienda
+
+    @Test
+    public void whenArrayListIsCreated_thenSizeIsZero() {
+        //inicialización
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //ejecución
+        int size=list.getSize();
+
+        //Validación
+        assertThat(list.getSize()).isZero();
+    }
+
+    public void givenANewArray_when_AddElement_thenSizeIsOne(){
+        //inicialización
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //ejecución
+        list.add(500);
+
+        //Validación
+        assertThat(list.get()).isEquialTo(1);
+        assertTrue();
+
+    }
+}*/
--- test/uaslp/ingenieria/labs/list/LinkedListTest.java
+++ test/uaslp/ingenieria/labs/list/LinkedListTest.java
@@ -1,2 +1,21 @@
-package uaslp.ingenieria.labs.list;public class LinkedListTest {
+package uaslp.ingenieria.labs.list;
+
+import com.sun.jdi.connect.spi.TransportService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LinkedListTest {
+    @Test
+    public void givenANewListIsCreated_ThenListIsEmpty(){
+        //Given:
+
+        //When:
+        ArrayList<Integer> lista =new ArrayList<>();
+        LinkedList<String> listaS=new LinkedList<>();
+
+        //Then:
+        assertEquals(0,lista.getSize());
+        assertEquals(0,listaS.getSize());
+    }
 }
